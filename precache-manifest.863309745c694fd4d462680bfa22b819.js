bf26e368882485c51eb342131047b43ad8def576
Ross Stolte <RossStolte@gmail.com>
2020-10-28 19:26:49 -0400

    Updates

---
+++ precache-manifest.863309745c694fd4d462680bfa22b819.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1730f769496a1e9c042b564fc56ae8",
+    "url": "/fantasy-outlook/index.html"
+  },
+  {
+    "revision": "53db9cacb52c050fc014",
+    "url": "/fantasy-outlook/static/js/2.1e8ad38a.chunk.js"
+  },
+  {
+    "revision": "67360280b1e728771bc49dea7ac92e3c",
+    "url": "/fantasy-outlook/static/js/2.1e8ad38a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d45266440f2e9ca874b",
+    "url": "/fantasy-outlook/static/js/main.9964bbce.chunk.js"
+  },
+  {
+    "revision": "84fb11303526d81481d7",
+    "url": "/fantasy-outlook/static/js/runtime-main.c6c62d84.js"
+  }
+]);
